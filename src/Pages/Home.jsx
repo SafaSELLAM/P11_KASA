f0f6af27716449294c6d909bcb733de61d6cc97e
SafaSELLAM <safa.sellam@outlook.fr>
2024-10-02 18:24:25 +0200

    Add Home page with cards

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,16 @@
+import  Banner  from '../Components/Banner/Banner' ;
+import homeBanner from '../assets/home_banner.png'
+import { Cards } from '../Components/Cards/Cards';
 const Home = () => {
-    return <h1>Accueil</h1>;
-  };
+  return (
+    <div className="Home">
+      <div className="bannerHome">
+        <Banner image={homeBanner} text="Chez vous, partout et ailleurs" />
+      </div>
+      <Cards />
+    </div>
+  );
+ 
+  }
   
   export default Home;
