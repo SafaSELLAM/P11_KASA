d195d006449e3ae6fa2d45742b2f72e1f1f5ee03
SafaSELLAM <safa.sellam@outlook.fr>
2024-10-02 18:27:25 +0200

    Add new routes to rooter

--- src/Components/Router.jsx
+++ src/Components/Router.jsx
@@ -1,11 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home";
-
+import About from "../Pages/About";
+import { Error } from "../Pages/Error/Error";
+import { Housings } from "../Pages/Housings/Housings";
 export const Router = () => {
     return (
         <>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/housing/:id" element={<Housings />} />
+                <Route path="/*" element={<Error />} />
             </Routes>
         </>
     );
