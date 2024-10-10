1cced125df740532ba83b3a75f3dabfe7d7df2bd
SafaSELLAM <safa.sellam@outlook.fr>
2024-10-10 17:44:59 +0200

    Add About infos

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,14 +1,25 @@
- import aboutBanner from '../assets/about_banner.jpg'
- import Banner from '../Components/Banner/Banner';
- const About = () =>{
-    return(
-        <div className="about">
-        <div className="bannerAbout">
-          <Banner image={aboutBanner} alt="bannière à propos" />
-        </div>
-       
+import aboutBanner from '../assets/about_banner.jpg'
+import Banner from '../Components/Banner/Banner';
+import { Collapse } from '../Components/Collapse/Collapse';
+import { aboutDatas } from '../datas/aboutDatas';
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="bannerAbout">
+        <Banner image={aboutBanner} alt="bannière à propos" />
       </div>
-    )
+      <div className=" column center">
+        {aboutDatas.map((abouts) => (
+          <Collapse
+            collapse_about="collapse_about"
+            key={abouts.id}
+            about={{ title: abouts.title, text: abouts.text, list: false }}
+          />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default About;
