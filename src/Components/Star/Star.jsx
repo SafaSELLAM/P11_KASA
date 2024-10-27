2887abe41f0cc1df79559377ce138a5443bf69ef
SafaSELLAM <safa.sellam@outlook.fr>
2024-10-27 16:22:42 +0100

    Changed star png into icons

--- src/Components/Star/Star.jsx
+++ src/Components/Star/Star.jsx
@@ -1,12 +1,9 @@
-import star from "../../assets/star_full.png"
-import starNull from "../../assets/star_empty.png"
 import './star.scss'
 export const Star = ({ isFilled }) => {
+    const filled = isFilled ? 'full' : 'empty';
     return (
-        <img
-            className="ratingStar"
-            src={isFilled ? star : starNull}
-            alt="ratings"
-        />
+        <i
+            className={`fa-solid fa-star ratingStar ${filled}`}
+        ></i>
     )
 }
